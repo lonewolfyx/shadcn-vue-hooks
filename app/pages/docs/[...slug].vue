bee1db4ab545e1266a4e778fb6460e7c70403355
lonewolfyx <877085893@qq.com>
2025-12-11 22:37:06 +0800

    refactor(docs): resiizes the maximum width of the document page from 2xl to 4xl

--- app/pages/docs/[...slug].vue
+++ app/pages/docs/[...slug].vue
@@ -7,7 +7,7 @@
         <div class="flex min-w-0 flex-1 flex-col">
             <div class="h-(--top-spacing) shrink-0" />
             <div
-                class="mx-auto flex w-full max-w-2xl min-w-0 flex-1 flex-col gap-8 px-4 py-6 text-neutral-800 md:px-0 lg:py-8 dark:text-neutral-300"
+                class="mx-auto flex w-full max-w-4xl min-w-0 flex-1 flex-col gap-8 px-4 py-6 text-neutral-800 md:px-0 lg:py-8 dark:text-neutral-300"
             >
                 <div class="flex flex-col gap-2">
                     <div class="flex flex-col gap-2">
